d550961f2154f3eafca71b2f635075e15487c0f5
Michel Nemnom <1678801+Pegase745@users.noreply.github.com>
2019-03-04 22:54:24 +0100

    Add npm provider in travis and update docker

--- bin/push-docker.sh
+++ bin/push-docker.sh
@@ -2,4 +2,9 @@
 
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 
-docker push pegase/shortnr
+docker build -t pegase/shortnr -f Dockerfile.prod .
+
+docker tag pegase/shortnr:latest pegase/shortnr:$TRAVIS_TAG
+
+docker push pegase/shortnr:latest
+docker push pegase/shortnr:$TRAVIS_TAG
