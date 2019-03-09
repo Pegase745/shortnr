b3ef1514f6623093e7edeebda39db45831258a4c
Michel Nemnom <1678801+Pegase745@users.noreply.github.com>
2019-03-09 19:01:02 +0100

    Redirect to bundle in production for client side routes

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,5 +1,6 @@
 import * as config from 'config';
 import * as fastify from 'fastify';
+import * as fs from 'fs';
 import * as path from 'path';
 
 import fastifyConfig from './config';
@@ -38,6 +39,16 @@ if (!isProduction) {
 if (isProduction) {
   // tslint:disable-next-line:no-var-requires
   server.use(require('serve-static')(path.join(__dirname, '../public')));
+
+  server.get('/*', (req, reply) => {
+    const stream = fs.createReadStream(
+      path.join(__dirname, '../public', 'index.html')
+    );
+    reply
+      .code(200)
+      .type('text/html')
+      .send(stream);
+  });
 }
 
 const host = config.get('server.host') as string;
