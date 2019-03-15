f4bbc71089892bbf0ba404a4209a4841fe9bc2d4
arnaud.dellinger <arnaud.dellinger@gandi.net>
2019-03-15 06:53:54 +0100

    Add URL formatting

---
+++ src/server/utils/url.ts
@@ -0,0 +1,17 @@
+import { parse as parseURL } from 'url';
+
+/**
+ * Format misspelled URLs.
+ * Will only prepend HTTP protocol if missing for now.
+ * @param {string} rawURL e.g. website.com, http://website.com
+ * @return {string}
+ */
+export function formatURL(rawURL: string): string {
+  const parsed = parseURL(rawURL);
+
+  if (!!parsed.protocol) {
+    return rawURL;
+  }
+
+  return `http://${rawURL}`;
+}
